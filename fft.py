b8542379c7fe5c45f74488acd1bb96add5fcab9e
Eric <eyang9001@gmail.com>
2019-04-11 13:49:45 -0500

    first version of fft

--- fft.py
+++ fft.py
@@ -1,14 +1,23 @@
+from pillow import Image
 import os
-import zipfile
-import gzip
-import shutil
+import numpy as np
+import nibabel as nib
+from MRI_FFT.TwoD import Direct2d
 
 folder = 'data'
 
+# Goes through all of the files that end in .gz
 for item in os.listdir(folder):
     if item.endswith(".gz"):
-        print('/' + folder + '/' + item)
-        with gzip.open(folder + '/' + item, 'rb') as f_in:
-            with open(folder + '/' + item[:-3], 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out)
+        img = nib.load('data/' + item)
+        # convert the image into a numpy array
+        a = np.array(img.dataobj)
+        print(a)
 
+        # do the reverse FFT
+        direct2dobject = Direct2d(a.shape)
+        result = direct2dobject.ifft2D(a)
+
+        # create new image
+        newimg = Image.fromarray(result.astype('uint8'))
+        newimg.show()
