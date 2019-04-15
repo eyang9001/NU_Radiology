b46f25d2d2e2a69b1136a411d728f2f3bb6aa29c
Eric <eyang9001@gmail.com>
2019-04-15 08:45:03 -0500

    Updating fft

--- fft.py
+++ fft.py
@@ -1,15 +1,15 @@
-from pillow import Image
+# from Pillow import Image
 import os
 import numpy as np
 import nibabel as nib
 from MRI_FFT.TwoD import Direct2d
 
-folder = 'data'
+folder = 'data/'
 
 # Goes through all of the files that end in .gz
 for item in os.listdir(folder):
     if item.endswith(".gz"):
-        img = nib.load('data/' + item)
+        img = nib.load(folder + item)
         # convert the image into a numpy array
         a = np.array(img.dataobj)
         print(a)
@@ -18,6 +18,6 @@ for item in os.listdir(folder):
         direct2dobject = Direct2d(a.shape)
         result = direct2dobject.ifft2D(a)
 
-        # create new image
-        newimg = Image.fromarray(result.astype('uint8'))
-        newimg.show()
+        # # create new image
+        # newimg = Image.fromarray(result.astype('uint8'))
+        # newimg.show()
