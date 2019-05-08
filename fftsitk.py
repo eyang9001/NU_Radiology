16a796042a87a7c8bcbab332fc9492b31e293a21
Eric <eyang9001@gmail.com>
2019-05-08 18:14:06 -0500

    Complete FFT

--- fftsitk.py
+++ fftsitk.py
@@ -12,28 +12,69 @@ import SimpleITK as sitk
 
 folder = 'data/'
 
-for item in os.listdir(folder):
-    if item.endswith(".gz"):
-        reader = sitk.ImageFileReader()
-        reader.SetImageIO("NiftiImageIO")
-        reader.SetFileName(folder + item)
-        image = reader.Execute()
+def fft_2d():
+    for item in os.listdir(folder):
+        if item.endswith(".gz"):
+            reader = sitk.ImageFileReader()
+            reader.SetImageIO("NiftiImageIO")
+            reader.SetFileName(folder + item)
+            image = reader.Execute()
 
-        # img1 = sitk.ReadImage(folder + item)  # alternative way to pull in image
+            # img1 = sitk.ReadImage(folder + item)  # alternative way to pull in image
 
-        # show image
-        # nda = sitk.GetArrayFromImage(image)
-        # plt.imshow(nda)
-        # plt.show()
+            # convert image into np array & perform fft
+            img = sitk.GetArrayFromImage(image)
+            f = np.fft.fft2(img)
+            fshift = np.fft.fftshift(f)
+            magnitude_spectrum = 20 * np.log(np.abs(fshift))
 
-        img = sitk.GetArrayFromImage(image)
-        f = np.fft.fft2(img)
-        fshift = np.fft.fftshift(f)
-        magnitude_spectrum = 20 * np.log(np.abs(fshift))
+            # display the slice & K-Space
+            plt.subplot(121), plt.imshow(img, cmap='gray')
+            plt.title('MRI Slice'), plt.xticks([]), plt.yticks([])
+            plt.subplot(122), plt.imshow(magnitude_spectrum, cmap='gray')
+            plt.title('K-Space'), plt.xticks([]), plt.yticks([])
+            plt.show()
 
-        fig, (ax1, ax2) = plt.subplots(1,2)
+def fft_3d():
+    for item in os.listdir(folder):
+        if item.endswith(".nii"):
+            reader = sitk.ImageFileReader()
+            reader.SetImageIO("NiftiImageIO")
+            reader.SetFileName(folder + item)
+            image = reader.Execute()
 
-        ax1.imshow(img, cmap='gray', interpolation='bicubic')
-        ax2.imshow(magnitude_spectrum, cmap='gray', interpolation='bicubic')
-        plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-        plt.show()
+            # img1 = sitk.ReadImage(folder + item)  # alternative way to pull in image
+
+            # convert image into np array & perform fft
+            img = sitk.GetArrayFromImage(image)
+            # print(img.shape)
+            orig_slice = img[100]
+            v = np.fft.fftn(img)
+            new_slice = v[100]
+            # print(v.shape)
+            # print(v[0][0])
+            magnitude_spectrum = 20 * np.log(np.abs(new_slice))
+            back_spectrum = np.fft.fftn(magnitude_spectrum)
+            back_img = 20 * np.log(np.abs(back_spectrum))
+
+            back_max = np.amax(back_spectrum)
+            back_min = np.amin(back_spectrum)
+
+            back_fft = np.fft.fftn(v)
+            back_slice = back_fft[100]
+
+            print(np.amax(orig_slice))
+
+            # plt.subplot(121), plt.imshow(back_img, cmap='gray')
+            # plt.title('MRI Slice'), plt.xticks([]), plt.yticks([])
+            plt.subplot(121), plt.imshow(orig_slice, cmap='gray')
+            plt.title('MRI Slice'), plt.xticks([]), plt.yticks([])
+            plt.subplot(122), plt.imshow(magnitude_spectrum, cmap='gray')
+            plt.title('K-Space'), plt.xticks([]), plt.yticks([])
+
+
+            plt.show()
+
+
+
+fft_3d()
