05af535de32cab8f7905b795a38428fd4adad8f5
Eric Yang <30561629+eyang9001@users.noreply.github.com>
2020-05-01 15:13:09 -0500

    Files for EC2

---
+++ Download_s3_files.py
@@ -0,0 +1,31 @@
+# To run in ec2 instance to download ALL files from s3 bucket
+import boto3
+import os
+# new directories
+directories = ['../originals', '../transforms', '../motion']
+for directory in directories:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+session = boto3.Session(
+    aws_access_key_id='AKIAIPNGQI7IMT2GV4CA',
+    aws_secret_access_key='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',
+)
+s3 = session.resource('s3')
+# s3.download_file('nuradmris', 'OBJECT_NAME', 'FILE_NAME')
+my_bucket = s3.Bucket('nuradmris')
+
+# my_bucket.download_file('originals/M01.nii', '../originals/M01.nii')
+for s3_object in my_bucket.objects.all():
+    # Need to split s3_object.key into path and file name, else it will give error file not found.
+    key = s3_object.key
+    trans_limit = 5 # limit to number of transformed mris downloaded
+    trans_cnt = 0
+    if not key.endswith('/'):
+        if key.startswith('transforms/') and trans_cnt < trans_limit:
+            print('Uploading: ' + key)
+            my_bucket.download_file(key, '../' + key)
+            trans_cnt += trans_cnt
+        elif not key.startswith('transforms/'):
+            print('Uploading: ' + key)
+            my_bucket.download_file(key, '../' + key)
